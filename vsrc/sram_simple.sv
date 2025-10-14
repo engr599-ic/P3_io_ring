5d95f112d6c5c1424a9a8a2786257e7959843ae2
Andrew Lukefahr <lukefahr@iu.edu>
2025-10-14 11:02:44 -0400

    added 4 element SRAM

--- vsrc/sram_simple.sv
+++ vsrc/sram_simple.sv
@@ -11,21 +11,46 @@ module sram_simple(
    
    output [31:0] mem_rdata,
    output mem_ready
-   
 );
-
-logic sram_write;
-assign sram_write = mem_valid && mem_wstrb[0];
 
 assign mem_ready = mem_valid;
 
-sram_8_1024_sky130A SRAM (
+sram_8_1024_sky130A SRAM_0 (
    .clk(clk),
    .csb0(rstn),
-   .web0(sram_write),
+   .web0(mem_valid && mem_wstrb[0]),
    .addr0(mem_addr[9:0]),
    .din0(mem_wdata[7:0]),
    .dout0(mem_rdata[7:0])
 ); 
+
+sram_8_1024_sky130A SRAM_1 (
+   .clk(clk),
+   .csb0(rstn),
+   .web0(mem_valid && mem_wstrb[1]),
+   .addr0(mem_addr[9:0]),
+   .din0(mem_wdata[15:8]),
+   .dout0(mem_rdata[15:8])
+); 
+
+sram_8_1024_sky130A SRAM_2 (
+   .clk(clk),
+   .csb0(rstn),
+   .web0(mem_valid && mem_wstrb[2]),
+   .addr0(mem_addr[9:0]),
+   .din0(mem_wdata[23:16]),
+   .dout0(mem_rdata[23:16])
+); 
+
+
+sram_8_1024_sky130A SRAM_3 (
+   .clk(clk),
+   .csb0(rstn),
+   .web0(mem_valid && mem_wstrb[3]),
+   .addr0(mem_addr[9:0]),
+   .din0(mem_wdata[31:24]),
+   .dout0(mem_rdata[31:24])
+); 
+
 
 endmodule
