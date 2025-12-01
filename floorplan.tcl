16ba7645f892cdf9b926a5a64e5364f02e953ea4
Andrew Lukefahr <lukefahr@iu.edu>
2025-12-01 17:25:09 -0500

    floorplan

--- floorplan.tcl
+++ floorplan.tcl
@@ -5,8 +5,8 @@ read_db dbs/syn_opt.db/
 
 create_net -physical -name VPWR -power
 create_net -physical -name VGND -ground
-create_net -physical -name VDDIO -power
-create_net -physical -name VSSIO -ground
+#create_net -physical -name VDDIO -power
+#create_net -physical -name VSSIO -ground
 
 # secondary IO ring pins
 create_net -physical -name VCCHIB
@@ -17,8 +17,6 @@ create_net -physical -name VDDA
 create_net -physical -name VSWITCH
 create_net -physical -name VSSA -ground
 #VSSD is VGND
-create_net -physical -name AMUXBUS_B
-create_net -physical -name AMUXBUS_A
 #create_net -physical -name VSSIO_Q
 #create_net -physical -name VDDIO_Q
 
@@ -33,7 +31,7 @@ set_db design_bottom_routing_layer met1
 
 # shoot for 50% utilization
 #create_floorplan -stdcell_density_size {1.0 0.5 2 2 2 2}
-read_floorplan thurber.fp
+read_floorplan top.fp
 
 
 # Ensure power pins are connected to power nets
@@ -55,8 +53,6 @@ connect_global_net VCCHIB -pin_base_name VCCHIB -all
 connect_global_net VDDA -pin_base_name VDDA -all
 connect_global_net VSWITCH -pin_base_name VSWITCH -all
 connect_global_net VSSA -type pg_pin -pin_base_name VSSA -inst_base_name *
-connect_global_net AMUXBUS_B -pin_base_name AMUXBUS_B -all
-connect_global_net AMUXBUS_A -pin_base_name AMUXBUS_A -all
 connect_global_net vssio -type pg_pin -pin_base_name VSSIO_Q -inst_base_name *
 connect_global_net vddio -type pg_pin -pin_base_name VDDIO_Q -inst_base_name *
 
@@ -66,33 +62,52 @@ update_lef_macro ./lef/sky130_ef_io__vssio_hvc_pad.lef
 update_lef_macro ./lef/sky130_ef_io__vccd_hvc_pad.lef
 update_lef_macro ./lef/sky130_ef_io__vssd_hvc_pad.lef
 
-## Add vertical and horizontal power straps
-add_stripes -nets {VPWR VGND} -layer met5 -direction horizontal -width 12 -spacing 12 -number_of_sets 3 -create_pins 1 -start_from left -start_offset 12 -stop_offset 12 -switch_layer_over_obs false -max_same_layer_jog_length 2 -pad_core_ring_top_layer_limit rdl -pad_core_ring_bottom_layer_limit li1 -block_ring_top_layer_limit rdl -block_ring_bottom_layer_limit li1 -use_wire_group 0 -snap_wire_center_to_grid none
-#
-add_stripes -nets {VPWR VGND} -layer met4 -direction vertical -width 12 -spacing 12 -number_of_sets 3  -create_pins 1 -start_from left -start_offset 12 -stop_offset 12 -switch_layer_over_obs false -max_same_layer_jog_length 2 -pad_core_ring_top_layer_limit rdl -pad_core_ring_bottom_layer_limit li1 -block_ring_top_layer_limit rdl -block_ring_bottom_layer_limit li1 -use_wire_group 0 -snap_wire_center_to_grid none
-
-add_rings -nets {vddio vssio} -type core_rings -follow core -layer {top met1 bottom met1 left met1 right met1} -width {top 1 bottom 1 left 1 right 1} -spacing {top 0.14 bottom 0.14 left 0.14 right 0.14} -offset {top 10 bottom 10 left 10 right 10} -center 0 -threshold 0 -jog_distance 0 -snap_wire_center_to_grid none
-
-#
-## Route the power and ground nets, the tool treats power and ground routes 
-## as special and seperate from signal routes
-route_special -connect core_pin \
-   -block_pin_target nearest_target \
-   -core_pin_target first_after_row_end \
-   -allow_jogging 1 \
-   -nets {VPWR VGND vddio vssio vccd vssd} \
-   -allow_layer_change 1
-
-add_well_taps -cell sky130_fd_sc_ms__tapvpwrvgnd_1 -cell_interval 60 -in_row_offset 30
-
 # redo IO filler
 delete_io_fillers -prefix FILLER
 add_io_fillers -cells sky130_ef_io__com_bus_slice_20um sky130_ef_io__com_bus_slice_10um sky130_ef_io__com_bus_slice_5um sky130_ef_io__com_bus_slice_1um
 add_io_fillers -cells sky130_ef_io__com_bus_slice_1um -fill_any_gap
 
+foreach INST [get_db insts *FILLER*] {
+    set_db [get_db $INST ] .place_status fixed
+}
+
+
+add_well_taps -cell sky130_fd_sc_ms__tapvpwrvgnd_1 -cell_interval 60 -in_row_offset 30
+
+create_pg_pin -name VPWR -net VPWR -geometry met5 2181.611 67.961 2205.745 85.558
+create_pg_pin -name VGND -net VGND -geometry met5 2634.903 1522.359 2657.127 1544.155
+
+## Add vertical and horizontal power straps
+#
+#
+
+#follow pins
+route_special -connect core_pin -layer_change_range { li1(1) rdl(7) } -block_pin_target nearest_target -core_pin_target none -allow_jogging 1 -crossover_via_layer_range { li1(1) rdl(7) } -nets { VPWR VGND } -allow_layer_change 1 -target_via_layer_range { li1(1) rdl(7) }
+
+add_stripes -nets {VPWR VGND} -layer met5 -direction horizontal -width 12 -spacing 12 -number_of_sets 3 -create_pins 1 -start_from left -start_offset 12 -stop_offset 12 -switch_layer_over_obs false -max_same_layer_jog_length 2 -pad_core_ring_top_layer_limit rdl -pad_core_ring_bottom_layer_limit li1 -block_ring_top_layer_limit rdl -block_ring_bottom_layer_limit li1 -use_wire_group 0 -snap_wire_center_to_grid none
+
+add_stripes -nets {VPWR VGND} -layer met4 -direction vertical -width 12 -spacing 12 -number_of_sets 3  -create_pins 1 -start_from left -start_offset 12 -stop_offset 12 -switch_layer_over_obs false -max_same_layer_jog_length 2 -pad_core_ring_top_layer_limit rdl -pad_core_ring_bottom_layer_limit li1 -block_ring_top_layer_limit rdl -block_ring_bottom_layer_limit li1 -use_wire_group 0 -snap_wire_center_to_grid none
+
+add_rings -nets {vddio vssio} -type core_rings -follow core -layer {top met1 bottom met1 left met1 right met1} -width {top 1 bottom 1 left 1 right 1} -spacing {top 0.14 bottom 0.14 left 0.14 right 0.14} -offset {top 10 bottom 10 left 10 right 10} -center 0 -threshold 0 -jog_distance 0 -snap_wire_center_to_grid none
+
+# general PG route
+route_special -connect {block_pin pad_pin pad_ring floating_stripe} -layer_change_range { li1(1) rdl(7) } -block_pin_target {nearest_target} -pad_pin_port_connect {all_port one_geom} -pad_pin_target {nearest_target} -floating_stripe_target {block_ring pad_ring ring stripe ring_pin block_pin followpin} -allow_jogging 1 -crossover_via_layer_range { li1(1) rdl(7) } -nets { VPWR VGND } -allow_layer_change 1 -block_pin use_lef -target_via_layer_range { li1(1) rdl(7) }
+
+# general PG route (again, focused on IO Power cells)
+route_special -connect {pad_pin} -layer_change_range { li1(1) rdl(7) } -block_pin_target {nearest_target} -pad_pin_port_connect {all_port one_geom} -pad_pin_target {nearest_target} -allow_jogging 1 -crossover_via_layer_range { li1(1) rdl(7) } -nets { VPWR VGND } -allow_layer_change 1 -target_via_layer_range { li1(1) rdl(7) }
+
+route_special -connect {pad_pin} -layer_change_range { li1(1) rdl(7) } -block_pin_target {nearest_target} -pad_pin_port_connect {all_port one_geom} -pad_pin_target {stripe} -allow_jogging 1 -crossover_via_layer_range { li1(1) rdl(7) } -nets { VPWR } -allow_layer_change 1 -target_via_layer_range { li1(1) rdl(7) }
+
+#
+## Route the power and ground nets, the tool treats power and ground routes 
+## as special and seperate from signal routes
+route_special -connect {block_pin pad_pin pad_ring core_pin floating_stripe} -layer_change_range { li1(1) rdl(7) } -block_pin_target {nearest_target} -pad_pin_port_connect {all_port one_geom} -pad_pin_target {nearest_target} -core_pin_target {none} -floating_stripe_target {block_ring pad_ring ring stripe ring_pin block_pin followpin} -allow_jogging 1 -crossover_via_layer_range { li1(1) rdl(7) } -nets { vddio vssio } -allow_layer_change 1 -block_pin use_lef -target_via_layer_range { li1(1) rdl(7) }
+
+
 set_db place_global_place_io_pins true
 
-#write_floorplan floorplan.fp
+write_floorplan top.fp
+write_io_file top.save.io
 
 # Save a database
 write_db -common dbs/floorplan.db
