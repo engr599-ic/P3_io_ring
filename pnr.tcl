21be0b9938c5d4b0fd31a3c6d1f38dace4408741
Jack E Clarke <jackclar@silo-rhel.luddy.indiana.edu>
2025-12-03 16:04:19 -0500

    Updated sram, removed bad ccopt commands, fixed mcon in floorplan

--- pnr.tcl
+++ pnr.tcl
@@ -2,9 +2,7 @@ set_multi_cpu_usage -remote_host 4 -local_cpu 4
 read_db dbs/floorplan.db/
 
 #don't route the amuxbus signals
-set_ccopt_property -net_type  amuxbus_a route_type none
 set_route_attributes -nets amuxbus_a -skip_routing true
-set_ccopt_property -net_type  amuxbus_b route_type none
 set_route_attributes -nets amuxbus_b -skip_routing true
 
 # Place the standard cells
